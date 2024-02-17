793693a96e956804839bc6f13ccf4c6f737ed2c8
Michael Faris <faris.gmichael@gmail.com>
2024-02-17 15:48:39 -0600

    Initial Commit

---
+++ WeatherApp/WeatherApp.h
@@ -0,0 +1,18 @@
+//
+//  WeatherApp.h
+//  WeatherApp
+//
+//  Created by Michael Faris on 2/17/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherApp.
+FOUNDATION_EXPORT double WeatherAppVersionNumber;
+
+//! Project version string for WeatherApp.
+FOUNDATION_EXPORT const unsigned char WeatherAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherApp/PublicHeader.h>
+
+
